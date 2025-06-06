063895ac7d0a412f0d8dd830c8978c641b9126d1
Nyaruke <k66561314@gmail.com>
2025-06-06 16:11:03 +0900

    added external mkbootimg

--- Android.mk
+++ Android.mk
@@ -3,3 +3,17 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),a04br3)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fstab.mt8167
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES := fstab.mt8167
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.mt8167.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES := init.mt8167.rc
+include $(BUILD_PREBUILT)
